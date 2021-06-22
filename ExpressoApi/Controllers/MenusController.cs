85aada67bbca3bd4839b1005ededad7ccb9a246d
Jason <jason.li@hexagon.com>
2021-06-22 16:25:00 +0800

    Add project files.

---
+++ ExpressoApi/Controllers/MenusController.cs
@@ -0,0 +1,47 @@
+﻿using ExpressoApi.Data;
+using ExpressoApi.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExpressoApi.Controllers
+{
+    public class MenusController : ApiController
+    {
+		private ExpressoDbContext m_oExpressoDbContext = new ExpressoDbContext();
+
+		[HttpGet]
+		public IHttpActionResult GetMenus()
+		{
+			var oMenus = m_oExpressoDbContext.Menus.Include("SubMenus");
+			//using (MySqlConnection connection = new MySqlConnection("Server = localhost; port = 3306; Database = ExpressoDb; uid = admin; pwd = 1234;"))
+			//{
+			//	using (ExpressoDbContext contextDB = new ExpressoDbContext())
+			//	{
+			//		contextDB.Database.CreateIfNotExists();
+			//		oMenus = contextDB.Menus.Include("SubMenus").ToList();
+			//	}
+			//}
+
+			return Ok(oMenus);
+		}
+
+		[HttpGet]
+		public IHttpActionResult GetMenu(int id)
+		{
+			var oMenu = m_oExpressoDbContext.Menus.Include("SubMenus").FirstOrDefault(m => m.Id == id);
+
+			if (oMenu == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(oMenu);
+		}
+	}
+}
---
+++ ExpressoApi/Controllers/ReservationsController.cs
@@ -0,0 +1,30 @@
+﻿using ExpressoApi.Data;
+using ExpressoApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExpressoApi.Controllers
+{
+    public class ReservationsController : ApiController
+    {
+		ExpressoDbContext m_oExpressoDbContext = new ExpressoDbContext();
+
+		[HttpPost]
+		public IHttpActionResult Post(Reservation reservation)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			m_oExpressoDbContext.Reservations.Add(reservation);
+			m_oExpressoDbContext.SaveChanges();
+
+			return StatusCode(HttpStatusCode.Created);
+		}
+    }
+}
